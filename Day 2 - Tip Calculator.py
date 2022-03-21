4ff84cbe163b68dff550a2dd0d3dbc99d79514d5
Jade <82993424+jjadeb@users.noreply.github.com>
2022-03-21 02:30:13 +0000

    add main() below

--- Day 2 - Tip Calculator.py
+++ Day 2 - Tip Calculator.py
@@ -5,7 +5,7 @@ def main() :
   
   cost = float(input("What was the total bill?\n"))
   tip = int(input("How much tip would you like to give?    10, 12, or 15?\n"))
-  num_people = int(input("How many people to split the bill?   \n"))
+  num_people = int(input("How many people to split the bill?\n"))
   
   total_cost = cost * (1 + tip * 0.01)
   cost_per_person = total_cost / num_people
@@ -13,4 +13,5 @@ def main() :
   
   print(f"Each person should pay: ${final_cost}")
 
+main()
 
