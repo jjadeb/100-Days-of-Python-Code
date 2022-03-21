f9f2a088c2ccc47fab671697b640e0830b77f6cd
Jade <82993424+jjadeb@users.noreply.github.com>
2022-03-21 02:28:04 +0000

    small edits to names/semi-colons

--- Day 2 - Tip Calculator.py
+++ Day 2 - Tip Calculator.py
@@ -1,16 +1,16 @@
 
 
 def main() :
-  print("Welcome to the tip calculator!");
+  print("Welcome to the tip calculator!")
   
-  bill = float(input("What was the total bill?\n"));
-  tip = int(input("How much tip would you like to give?    10, 12, or 15?\n"));
-  people = int(input("How many people to split the bill?   \n"));
+  cost = float(input("What was the total bill?\n"))
+  tip = int(input("How much tip would you like to give?    10, 12, or 15?\n"))
+  num_people = int(input("How many people to split the bill?   \n"))
   
-  total_bill = bill * (1 + tip * 0.01);
-  bill_per_person = total_bill / people;
-  final_cost = "{0.2f}".format(bill_per_person);
+  total_cost = cost * (1 + tip * 0.01)
+  cost_per_person = total_cost / num_people
+  final_cost = "{:.2f}".format(cost_per_person)
   
-  print(f"Each person should pay: ${final_cost}");
+  print(f"Each person should pay: ${final_cost}")
 
 
