e309b182b92ab687d78644bc9c95b601f7e96853
Jade <82993424+jjadeb@users.noreply.github.com>
2022-03-23 02:53:39 +0000

    add main hello world

--- Day_2_Tip_Calculator.py
+++ Day_2_Tip_Calculator.py
@@ -1,16 +1,15 @@
+def tipCalc():
+    print("Welcome to the tip calculator!")
 
+    cost = float(input("What was the total bill?\n"))
+    tip = int(
+        input("How much tip would you like to give?    10, 12, or 15?\n"))
+    num_people = int(input("How many people to split the bill?\n"))
 
-def tipCalc() :
-  print("Welcome to the tip calculator!")
-  
-  cost = float(input("What was the total bill?\n"))
-  tip = int(input("How much tip would you like to give?    10, 12, or 15?\n"))
-  num_people = int(input("How many people to split the bill?\n"))
-  
-  total_cost = cost * (1 + tip * 0.01)
-  cost_per_person = total_cost / num_people
-  final_cost = "{:.2f}".format(cost_per_person)
-  
-  print(f"Each person should pay: ${final_cost}")
+    total_cost = cost * (1 + tip * 0.01)
+    cost_per_person = total_cost / num_people
+    final_cost = "{:.2f}".format(cost_per_person)
 
+    print(f"Each person should pay: ${final_cost}")
 
+print("Hello World")
