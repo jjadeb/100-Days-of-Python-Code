41e5a564a2436ea257a331e4cce1eddd64fbcfa4
Jade Bouchard <jtakbouchard@gmail.com>
2022-08-09 17:46:07 -0700

    Started Day 17 quiz game

---
+++ Day_17_Quiz_Game/main.py
@@ -0,0 +1,29 @@
+from data import question_data
+from quiz_brain import QuizBrain
+from question_model import Question
+import os
+
+
+question_bank = []
+for q in question_data:
+    question = Question(q["text"], q["answer"])
+    question_bank.append(question)
+
+
+def play_quiz_game():
+    print("Welcome to my Europe Quiz!")
+    quiz = QuizBrain(question_bank)
+    while quiz.still_has_questions():
+        quiz.next_question()
+    yes_no = input("Would you like to play again? (y/n): ").lower()
+    if yes_no == "y":
+        os.system("clear")
+        play_quiz_game()
+    else:
+        quit(0)
+
+
+play_quiz_game()
+
+
+
