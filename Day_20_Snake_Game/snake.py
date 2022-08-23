b5549167979b3a70f8de5fa7cb73d5cac7365181
Jade Bouchard <jtakbouchard@gmail.com>
2022-08-23 10:24:45 -0700

    Created snake that can be controlled with keyboard

---
+++ Day_20_Snake_Game/snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """Creates snake on screen"""
+        position = 0
+        for t in range(0, 3):
+            t = Turtle()
+            t.shape("square")
+            t.fillcolor("white")
+            t.penup()
+            t.goto((position * -20, 0))
+            position += 1
+            self.segments.append(t)
+
+    def move(self):
+        "Moves snake forward"
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_place = self.segments[seg_num - 1]
+            x = new_place.xcor()
+            y = new_place.ycor()
+            self.segments[seg_num].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """Turns snake upwards, if not facing downwards"""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """Turns snake downwards, if not facing upwards"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """Turns snake to the left, if not facing right"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """Turns snake right, if not facing left"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
